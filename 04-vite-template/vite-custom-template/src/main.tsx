4271790c9419126133c38cebb72b89f82ceb2733
kindtiger <seonbeom2@gmail.com>
2025-07-07 15:29:27 +0900

    learn. vite-custom-template

---
+++ 04-vite-template/vite-custom-template/src/main.tsx
@@ -0,0 +1,20 @@
+
+// 17+
+
+/* 
+
+npm i vite -D
+npm i react react-dom
+npm i @types/react @types/react-dom @types/node
+npm i @vitejs/plugin-react
+npm init @eslint/config@latest
+npm i -D eslint-plugin-react-hooks eslint-plugin-react-refresh
+
+ */
+
+
+
+
+
+
+
