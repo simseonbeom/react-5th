3aa36a0ea14ee98bcd2439a8c32ce711448ace36
kindtiger <seonbeom2@gmail.com>
2025-07-07 12:28:43 +0900

    learn. component-with-props finish

--- 02-component-with-props/src/main.js
+++ 02-component-with-props/src/main.js
@@ -2,7 +2,7 @@ import React, { createElement as h } from "./lib/react.js";
 import ReactDOM, { createRoot } from "./lib/react-dom.js";
 import { PlanetPage } from "./pages/PlanetPage.js";
 import { _PlanetPage } from "./pages/_PlanetPage.js";
-import { AvatarPage } from "./pages/AvatarPage.js";
+import AvatarPage from "./pages/AvatarPage.js";
 
 
 /* class syntax  */
