2bdbfea03c1b8fd2516f2208af3940972ef17b9c
kindtiger <seonbeom2@gmail.com>
2025-07-08 12:12:15 +0900

    learn. 조건부 렌더링 finish

--- 05-html-vs-jsx-markup/src/learn/ConditionalRendering.tsx
+++ 05-html-vs-jsx-markup/src/learn/ConditionalRendering.tsx
@@ -2,6 +2,7 @@ import reactImagePath from "@/assets/react.svg?url";
 import viteImagePath from "@/assets/vite.svg?url";
 import nextJsImagePath from "@/assets/next-js.svg?url";
 import kakaoTalkImagePath from "@/assets/kakao-talk.svg?url";
+import isTrueOrFalse from "@/utils/isTrueOrFalse";
 
 interface Props {
   imageType: string;
@@ -32,10 +33,14 @@ function ConditionalRendering({ imageType }: Props) {
     default:
       printText = "허용된 이미지 타입이 존재하지 않습니다.";
   }
+  const tf = isTrueOrFalse();
+  const spinnerOrVite = tf ? 
+  (<img src="/icons/spinner.svg" alt="로딩 중..." />):
+  (<img src="vite.svg" alt="vite logo" />)
 
   return (
     <>
-      <dt>조건부 렌더링(conditional rendering)</dt>
+      <dt>조건부 렌더링(conditional rendering) ({tf && '스피너 표시'})</dt>
       <dd>
         <p>이미지 타입(image type)에 따라 렌더링 여부를 결정합니다.</p>
         <div className="conditionalRendering">
@@ -45,9 +50,8 @@ function ConditionalRendering({ imageType }: Props) {
       </dd>
       <dd>
         <p>spinner 또는 vite 이미지가 랜덤으로 화면에 렌더링 되도록 합니다.</p>
-        <div className="conditionalRendering">
-          <img src="/icons/spinner.svg" alt="" />
-        </div>
+        
+        <div className="conditionalRendering"> {spinnerOrVite} </div>
       </dd>
     </>
   );
