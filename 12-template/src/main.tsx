1786039989e365238ddf351f33862136d416c31e
kindtiger <seonbeom2@gmail.com>
2025-07-14 08:43:34 +0900

    add. 학습 진도 추가

---
+++ 12-template/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import '@/styles/main.css';
+
+
+const container = document.getElementById('react-app');
+
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+        <main>main</main>
+    </StrictMode>
+  );
+} else {
+  console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
+}
