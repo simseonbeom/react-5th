7df3d462dd2dcf0960ef3d5e061ad6e271a9307f
kindtiger <seonbeom2@gmail.com>
2025-07-14 12:50:36 +0900

    learn. useEffect & customHook

--- 11-fetching-data/src/main.tsx
+++ 11-fetching-data/src/main.tsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react';
+// import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@/styles/main.css';
+import Effect from './components/effect/Effect';
 
 
 const container = document.getElementById('react-app');
 
 if (container) {
   createRoot(container).render(
-    <StrictMode>
-        <main>main</main>
-    </StrictMode>
+      <Effect />
   );
 } else {
   console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
