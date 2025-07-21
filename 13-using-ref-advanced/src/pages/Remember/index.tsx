96071ea33b85f283c66281bbeb233d1411c800fc
kindtiger <seonbeom2@gmail.com>
2025-07-21 10:13:57 +0900

    learn. useRef vs useState

--- 13-using-ref-advanced/src/pages/Remember/index.tsx
+++ 13-using-ref-advanced/src/pages/Remember/index.tsx
@@ -1,14 +1,21 @@
 
-import { useId, useRef } from 'react'
+import { useId, useRef, useState } from 'react'
 import S from './Remember.module.css'
 
 function Remember() {
 
   const refInputId = useId()
+  const stateInputId = useId()
   
   // useRef() 훅 => 메모이제이션, 외부에 데이터 기억 저장/읽기
   // 현재(current) 기억된 값이 변경되더라도 기억은 하지만, 리-렌더 요청은 안함 
   const messageRef = useRef('메시지를 기억합니다.');
+  const reRender = useState(0)[1];
+  
+  // 함수 내 데이터 기억하는 방법
+  // useState() 훅 함수 => 메모이제이션, 외부에 데이터 기억 저장/읽기
+  // 필연적으로 리액트에게 다시 컴포넌트를 리-렌더 하도록 요청
+  const [message,setMessage] = useState('');
   
   const handleRefChange = ({target:{value}}:{target:{value:string}}) => {
 
@@ -19,6 +26,16 @@ function Remember() {
 
     messageRef.current = value;
     console.log('ref 참조 객체의 현재 값이 변경됩니다.');
+  }
+
+  const handleStateChange = ({target:{value}}:{target:{value:string}}) => {
+    setMessage(value)
+  }
+
+  const handleReRender = () => {
+
+    reRender((r)=> --r);
+    
   }
 
   const handleClick = () => {
@@ -44,15 +61,27 @@ function Remember() {
           onChange={handleRefChange}
         />
       </div>
+      <div className={S.control}>
+        <label htmlFor={stateInputId} className="a11y-hidden">메시지 State</label>
+        <input 
+          id={stateInputId}
+          type="text" 
+          value={message}
+          onChange={handleStateChange}
+        />
+      </div>
 
       <div className={S.group}>
         <button type="button" onClick={handleClick}>메시지 확인</button>
-        <button type="button">다시 렌더링</button>
+        <button type="button" onClick={handleReRender}>다시 렌더링</button>
       </div>
 
       <div className={S.result}>
         <h3>Ref 메시지 값 : </h3>
         <p>{messageRef.current}</p>
+        <hr />
+        <h3>State 메시지 값 : </h3>
+        <p>{message}</p>
       </div>
 
     </main>
