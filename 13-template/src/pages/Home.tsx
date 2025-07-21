7a2b49e528ce68cd4c68108f3e218c8c227919c0
kindtiger <seonbeom2@gmail.com>
2025-07-21 09:16:32 +0900

    add. 13 template

---
+++ 13-template/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+import S from './Home.module.css'
+
+
+function Home() {
+  return (
+    <main className={S.container}>
+        main
+    </main>        
+  )
+}
+export default Home
---
+++ 13-template/src/router/router.tsx
@@ -0,0 +1,24 @@
+import Home from "@/pages/Home";
+
+
+
+
+
+export const routes = [
+  {
+    title:'리액트 애니메이션',
+    path:'/',
+    element: <Home />
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
