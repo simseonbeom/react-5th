c8d6880a6e3642f34626b7f018835ce84819683a
kindtiger <seonbeom2@gmail.com>
2025-07-21 11:45:43 +0900

    learn. useRef with RefCallback

--- 13-using-ref-advanced/src/pages/AccessDom/components/CardItem.tsx
+++ 13-using-ref-advanced/src/pages/AccessDom/components/CardItem.tsx
@@ -2,6 +2,7 @@ import type { Movie_list } from "../type"
 import S from '../style.module.css'
 import VanillaTilt from "vanilla-tilt";
 import { TILT_CONFIG } from "./tiltConfig";
+import { useRef } from "react";
 
 type Props = {
   item:Movie_list;
@@ -11,6 +12,7 @@ type Props = {
 function CardItem({item, popup}: Props) {
 
   const { href, label, images } = item;
+  const titleRef = useRef<HTMLParagraphElement | null>(null)
 
   const refCallback = (el:HTMLAnchorElement /* domElementNode */) => {
 
@@ -27,23 +29,34 @@ function CardItem({item, popup}: Props) {
   // cva
 
   // 카드에 마우스 올렸을 때 label이 보이게 
-  // 카드에 마우스 떠났을 때 label이 안 보이게 
+  const handleEnter = () => {
+  // label 을 잡아서 style 수정
+    const title = titleRef.current!;
+    title.style.opacity = '1';
+  }
 
+  // 카드에 마우스 떠났을 때 label이 안 보이게 
+  const handleLeave = () => {
+    const title = titleRef.current!;
+    title.style.opacity = '0';
+  }
 
   return (
     <a 
       ref={refCallback}
       href={href}
       title={label}
-      aria-label={label}  
+      aria-label={label}
+      onPointerEnter={handleEnter}
+      onPointerLeave={handleLeave}
     >
 
     <figure className={cardClassNames}>
       <div className={S.wrapper}>
         <img src={images.src} alt="" />
       </div>
-      <p>{label}</p>
-      <img  className={S.character} src={images.character} alt="" />
+      <p ref={titleRef}>{label}</p>
+      <img className={S.character} src={images.character} alt="" />
     </figure>
 
     </a>
