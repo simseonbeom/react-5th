9b719d1cd15a4b48f1dbe7397c9e6880072f9284
kindtiger <seonbeom2@gmail.com>
2025-08-14 09:50:51 +0900

    learn. tailwind theme, base

--- 15-style-with-tailwind/src/App.tsx
+++ 15-style-with-tailwind/src/App.tsx
@@ -1,4 +1,5 @@
 
+import Playground from './components/Playground'
 import '/src/styles/style.css'
 
 
@@ -7,7 +8,11 @@ function App() {
   return (
     <div>
       <h1 className='flex justify-center items-center bg-indigo-500 text-white px-4 py-2 text-shadow-lg'>hello tailwind</h1>
+      
+      <Playground />
     </div>
   )
 }
 export default App
+
+
---
+++ 15-style-with-tailwind/src/components/Playground.tsx
@@ -0,0 +1,14 @@
+function Playground() {
+  return (
+    <div>
+
+      <div className="bg-amber-400 sm:bg-red-400 text-neutral-800 px-l py-m">playground</div>
+      <a href="#">links</a>
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+
+    </div>
+  )
+}
+export default Playground
