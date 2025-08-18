b98985e333562f576b3a747d088a1543d91a3a20
kindtiger <seonbeom2@gmail.com>
2025-08-18 14:50:23 +0900

    learn. page css 분리

--- 16-advanced-state-management/src/components/GlobalNav.tsx
+++ 16-advanced-state-management/src/components/GlobalNav.tsx
@@ -1,27 +1,52 @@
+/* 
+w-1/2 -> width:50%
+w-1/3 -> width: 33.333333%
+w-2/3 -> width: 66.666666%
+w-4/5 -> width: 80%
+w-11/12 -> width: 91.666667%
+*/
+
 import { navigationItems } from "@/router/router"
+import tw from "@/utils/tw";
 import { useState } from "react"
+import { NavLink } from "react-router";
 
 
 function GlobalNav() {
 
-  
-  
   const [items] = useState(navigationItems);
   
-  console.log( items );
-  
+  const baseNavClasses = 
+    'text-sm py-2 px-4 text-indigo-800/70 rounded-full hover:text-indigo-800'
 
   return (
     <nav className="bg-white">
       <h2 className="sr-only">페이지 탐색</h2>
-      <ul>
-        {/* {
-          navList.map...
-        } */}
-        <li><a href="#">menu01</a></li>
-        <li><a href="#">menu02</a></li>
-        <li><a href="#">menu03</a></li>
-      </ul>
+      {
+        items.length > 0 && (
+          <ul className="py-4 w-4/5 max-w-6xl flex justify-center gap-4 mx-auto">
+            {
+              items.map((item, index)=> (
+                <li key={item.path ?? index}>
+                  <NavLink 
+                    to={item.path}
+                    end={item.path?.endsWith('/') ?? false}
+                    className={({isActive})=>{
+                      return isActive ? tw(
+                        baseNavClasses,
+                        'text-indigo-900 bg-indigo-100/40 border border-indigo-100/70 font-semibold'
+                      ) : baseNavClasses
+                    }}
+                  >
+                    {item.text}
+                  </NavLink>
+                </li>
+               )
+              )
+            }
+          </ul>
+        )
+      }
     </nav>
   )
 }
--- 16-advanced-state-management/src/pages/Home/index.tsx
+++ 16-advanced-state-management/src/pages/Home/index.tsx
@@ -1,7 +1,12 @@
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <section id="page">
+      <div className="learn">
+        <h1>앱 글로벌 상태 관리 with Zustand</h1>
+        <p>Zustand 라이브러리를 사용해 앱 또는 컴포넌트의 상태를 효과적으로 관리하는 방법을 학습합니다.</p>
+      </div>
+    </section>
   )
 }
 
